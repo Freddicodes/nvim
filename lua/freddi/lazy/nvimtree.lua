a7ec9369c809702fd713456ca5090f715604cbf2
Jan <jan.draeger@student.uni-augsburg.de>
2025-05-14 13:07:32 +0200

    temp remove nvimtree
    remove nvim tree due to issue when closing nvim

--- lua/freddi/lazy/nvimtree.lua
+++ lua/freddi/lazy/nvimtree.lua
@@ -1,29 +1,30 @@
-return {
+return {}
+--[[ return {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
+        "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup(
-        {
-          filters = {
-            dotfiles = false,
-            git_ignored = false
-          },
-          actions = { -- close the tree on file open
-            open_file = {
-              quit_on_open = true,
-            }
-          }
-        })
-       -- Close nvim-tree when quitting Neovim
-       vim.api.nvim_create_autocmd("QuitPre", {
-          callback = function()
-              local api = require("nvim-tree.api")
-              api.tree.close()
-          end,
+        require("nvim-tree").setup(
+            {
+                filters = {
+                    dotfiles = false,
+                    git_ignored = false
+                },
+                actions = { -- close the tree on file open
+                    open_file = {
+                        quit_on_open = true,
+                    }
+                },
+            })
+        -- Close nvim-tree when quitting Neovim
+        vim.api.nvim_create_autocmd("QuitPre", {
+            callback = function()
+                local api = require("nvim-tree.api")
+                api.tree.close()
+            end,
         })
     end,
-  }
+} ]]

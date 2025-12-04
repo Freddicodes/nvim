fd424ba381fc19143f17baefe6fa3e0ce5cde542
Jan Frederik Draeger <jan.draeger@studen.uni-augsburg.de>
2025-12-04 14:19:13 +0100

    Update config

--- lua/nconf/remap.lua
+++ lua/nconf/remap.lua
@@ -57,9 +57,17 @@ vim.keymap.set("n", "<leader>c", ":nohlsearch<CR>", { desc = "Clear search highl
 
 -- telescope mapings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-vim.keymap.set('n', ';', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Search git files" })
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, {desc = "Live grep" })
+ --vim.keymap.set('n', ';', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Search buffers" })
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {desc = "Search diagnostics" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Search help tags" })
+
+-- Diagnostics
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Diagnostics to location list" })
 
